7a12a67f42aa58860ec5a6f721677385d94fcc10
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-11-01 04:52:57 -0500

    Changed Docker version from Debian to Ubuntu

--- setup.sh
+++ setup.sh
@@ -10,23 +10,25 @@ git clone https://github.com/rappi-clone/orders.git
 git clone https://github.com/rappi-clone/stores.git
 git clone https://github.com/rappi-clone/shopping-carts.git
 
-#Docker for debian installation
+#Docker for Ubuntu installation
 sudo apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 sudo apt update
 sudo apt install docker-ce
+sudo docker run hello-world
 
 #Install docker-compose for linux
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
