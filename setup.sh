e293cba480da74a64de6d3ed199dd16a8a69c23b
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-11-01 04:15:20 -0500

    Added commands to install docker-compose and run it automatically afterwards

--- setup.sh
+++ setup.sh
@@ -28,5 +28,12 @@ sudo add-apt-repository \
 sudo apt update
 sudo apt install docker-ce
 
+#Install docker-compose for linux
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#Starts docker-compose
+cd docker
+sudo docker-compose up
 
 
