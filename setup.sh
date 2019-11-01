7505c4f5aa2c59d18a70380f573b77056ff8c82a
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-11-01 04:02:12 -0500

    Added a setup script that clones all the repositories, installs git and installs docker automatically

---
+++ setup.sh
@@ -0,0 +1,30 @@
+sudo apt update
+sudo apt install git
+
+#git clone https://github.com/rappi-clone/docker.git
+git clone https://github.com/rappi-clone/clients-front.git
+git clone https://github.com/rappi-clone/users.git
+git clone https://github.com/rappi-clone/orders.git
+git clone https://github.com/rappi-clone/stores.git
+git clone https://github.com/rappi-clone/shopping-carts.git
+
+#Docker for debian installation
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt install docker-ce
+
+
+
